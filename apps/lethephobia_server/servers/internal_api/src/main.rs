9696d65d520ffddce7223e088dcea079f24a42d0
pbandj082 <pbandj082@gmail.com>
2025-10-05 17:49:24 +0900

    chore: letheserver base

--- apps/lethephobia_server/servers/internal_api/src/main.rs
+++ apps/lethephobia_server/servers/internal_api/src/main.rs
@@ -1,13 +1,18 @@
 use axum::{Router, routing::get};
 use tokio::net::TcpListener;
 
+use internal_api::states::AppConfig;
+use internal_api::states::AppState;
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let router = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .with_state(AppState {
+            app_config: AppConfig,
+        });
 
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
